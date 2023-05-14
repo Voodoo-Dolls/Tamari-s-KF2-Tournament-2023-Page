a670cb2170290d9030e38f4ef0857d0035fac0cd
Michael Dam <92550503+Voodoo-Dolls@users.noreply.github.com>
2023-05-14 00:26:46 -0600

    added nav indicator

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
 var welcome = document.getElementById("welcome");
 var about = document.getElementById("about");
+var objective = document.getElementById("objective");
 var features = document.getElementById("features");
 var register = document.getElementById("register");
 var test = document.getElementById("test");
 var welcomeAnchor = document.getElementById("header1");
 var aboutAnchor = document.getElementById("header2");
-var featuresAnchor = document.getElementById("header3");
-var registerAnchor = document.getElementById("header4");
+var objectiveAnchor = document.getElementById("header3");
+var featuresAnchor = document.getElementById("header4");
+var registerAnchor = document.getElementById("header5");
 var global = "hello";
 
 let myHeaders =[
     [welcome, welcomeAnchor],
     [about, aboutAnchor],
+    [objective, objectiveAnchor],
     [features, featuresAnchor],
     [register, registerAnchor]
 ];
 
 function myFunction(){ 
-    welcomeAnchor.innerHTML= myHeaders[0][0].getBoundingClientRect().y;
-    aboutAnchor.innerHTML= myHeaders[1][0].getBoundingClientRect().y;
-    featuresAnchor.innerHTML= myHeaders[2][0].getBoundingClientRect().y;
 
     for (let i = 0; i < myHeaders.length; i++){
-
-        if (myHeaders[i][0].getBoundingClientRect().y < 400 && myHeaders[i][0].getBoundingClientRect().y > 0){
-
-            myHeaders[i][1].style.color="yellow";
-
-        }
-        else{
+        if (myHeaders[i][0].getBoundingClientRect().y < 200 && myHeaders[i][0].getBoundingClientRect().y > 0){
             myHeaders[i][1].style.color="red";
         }
 
+        else if (myHeaders[i][0].getBoundingClientRect().bottom < 0){
+            myHeaders[i][1].style.color="white";
+        }
+
+        else{
+            myHeaders[i][1].style.color="white";
+        }
+  
      
         
 
