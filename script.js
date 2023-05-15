bd03ebee0937328c40c4c0f1abff0abe1df29f1b
Michael Dam <92550503+Voodoo-Dolls@users.noreply.github.com>
2023-05-15 01:57:50 -0600

    added timer

--- script.js
+++ script.js
@@ -18,6 +18,61 @@ background.style.backgroundImage= background_images[current.innerHTML];
 
 
 
+////////// CONFIGURE THE SCRIPT HERE //////////////////
+
+var month = '*';     //  '*' for next month, '0' for this month or 1 through 12 for the month 
+var day = '-10';       //  Offset for day of month day or + day  
+var hour = 23;        //  0 through 23 for the hours of the day
+var tz = -8;         //  Offset for your timezone in hours from UTC
+var lab = 'tzpst';    //  The id of the page entry where the timezone countdown is to show. For example on this page, <span id="tzpst"></span>
+
+function start() {displayTZCountDown(setTZCountDown(month,day,hour,tz),lab);}
+
+    // **    The start function can be changed if required   **
+window.onload = start;
+
+////////// DO NOT EDIT PAST THIS LINE //////////////////
+
+function setTZCountDown(month,day,hour,tz) 
+{
+var toDate = new Date();
+if (month == '*')toDate.setMonth(toDate.getMonth() + 1);
+else if (month > 0) 
+{ 
+if (month <= toDate.getMonth())toDate.setYear(toDate.getYear() + 1);
+toDate.setMonth(month-1);
+}
+if (day.substr(0,1) == '+') 
+{var day1 = parseInt(day.substr(1));
+toDate.setDate(toDate.getDate()+day1);
+} 
+else{toDate.setDate(day);
+}
+toDate.setHours(hour);
+toDate.setMinutes(0-(tz*60));
+toDate.setSeconds(0);
+var fromDate = new Date();
+fromDate.setMinutes(fromDate.getMinutes() + fromDate.getTimezoneOffset());
+var diffDate = new Date(0);
+diffDate.setMilliseconds(toDate - fromDate);
+return Math.floor(diffDate.valueOf()/1000);
+}
+function displayTZCountDown(countdown,tzpst) 
+{
+if (countdown < 0) document.getElementById(tzpst).innerHTML = "Sorry, you are too late."; 
+else {var secs = countdown % 60; 
+if (secs < 10) secs = '0'+secs;
+var countdown1 = (countdown - secs) / 60;
+var mins = countdown1 % 60; 
+if (mins < 10) mins = '0'+mins;
+countdown1 = (countdown1 - mins) / 60;
+var hours = countdown1 % 24;
+var days = (countdown1 - hours) / 24;
+document.getElementById(tzpst).innerHTML = days + " day" + (days == 1 ? '' : 's') + ' + ' +hours+ 'h : ' +mins+ 'm : '+secs+'s';
+setTimeout('displayTZCountDown('+(countdown-1)+',\''+tzpst+'\');',999);
+}
+}
+//-->
 
 
 
@@ -45,47 +100,3 @@ background.style.backgroundImage= background_images[current.innerHTML];
 
 
 
-
-// var welcome = document.getElementById("welcome");
-// var about = document.getElementById("about");
-// var objective = document.getElementById("objective");
-// var features = document.getElementById("features");
-// var register = document.getElementById("register");
-// var test = document.getElementById("test");
-// var welcomeAnchor = document.getElementById("header1");
-// var aboutAnchor = document.getElementById("header2");
-// var objectiveAnchor = document.getElementById("header3");
-// var featuresAnchor = document.getElementById("header4");
-// var registerAnchor = document.getElementById("header5");
-// var global = "hello";
-
-// let myHeaders =[
-//     [welcome, welcomeAnchor],
-//     [about, aboutAnchor],
-//     [objective, objectiveAnchor],
-//     [features, featuresAnchor],
-//     [register, registerAnchor]
-// ];
-
-// function myFunction(){ 
-
-//     for (let i = 0; i < myHeaders.length; i++){
-//         if (myHeaders[i][0].getBoundingClientRect().y < 200 && myHeaders[i][0].getBoundingClientRect().y > 0){
-//             myHeaders[i][1].style.color="red";
-//         }
-
-//         else if (myHeaders[i][0].getBoundingClientRect().bottom < 0){
-//             myHeaders[i][1].style.color="white";
-//         }
-
-//         else{
-//             myHeaders[i][1].style.color="white";
-//         }
-  
-     
-        
-
-           
-//     };   
-    
-// };
